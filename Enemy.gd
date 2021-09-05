2a2c183f8e2b51c0f12ccbfe40fbb73a24f2089d
Bryden Frizzell <bryden.frizzell@gmail.com>
2021-09-05 13:53:11 +1200

    Made enemy be mannequin

--- Enemy.gd
+++ Enemy.gd
@@ -10,13 +10,12 @@ var health = 100.0
 
 const attackTime = 1.0
 # attack anim time
-const attackCycleLength = 0.1
+const attackCycleLength = 0.2
 var currAttackTime = 0.0
 const damage = 3.0
 const jumpHeight = 0.3
 
 
-onready var transformOrigin = $MeshInstance.transform.origin
 onready var wall = get_node("../Terrain/Wall/WallOmniCollider")
 onready var world = get_node("/root/World")
 
@@ -24,6 +23,10 @@ onready var world = get_node("/root/World")
 func _ready():
 	contact_monitor = true
 	contacts_reported = 5
+	look_at(global_transform.origin + Vector3(1.0,0.0,0.0), Vector3.UP)
+	$Mannequiny.set_is_moving(true)
+	$Mannequiny.set_move_direction(Vector3(1.0,0.0,0.0))
+	$Mannequiny.transition_to(Mannequiny.States.RUN)
 	pass # Replace with function body.
 
 # used to set max velocity
@@ -34,9 +37,11 @@ func _integrate_forces(state):
 	
 
 func attack_anim(time):
-	time = min(time, attackCycleLength)
-	var height = sin((time / attackCycleLength) * PI) * jumpHeight
-	$MeshInstance.transform.origin = transformOrigin + Vector3(0.0, height, 0.0)
+	if time > attackCycleLength:
+		$Mannequiny.set_is_moving(false)
+		$Mannequiny.set_move_direction(Vector3(0.0,0.0,0.0))
+		$Mannequiny.transition_to(Mannequiny.States.IDLE)
+	#$MeshInstance.transform.origin = transformOrigin + Vector3(0.0, height, 0.0)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -47,6 +52,9 @@ func _process(delta):
 	if currAttackTime <= 0.0:
 		if wall in get_colliding_bodies():
 			world.damage_wall(damage)
+			$Mannequiny.set_is_moving(false)
+			$Mannequiny.set_move_direction(Vector3(0.0,0.0,0.0))
+			$Mannequiny.transition_to(Mannequiny.States.AIR)
 			currAttackTime = attackTime
 	#move_and_collide(Vector3(-speed * delta, 0.0, 0.0 ))
 	add_force(Vector3(-1.0, 0.0, 0.0) * delta * accel, Vector3(0,0,0))
